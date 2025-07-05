d45bd1d910b7d57c30f588cae3cdbfac5ab22919
johnbarn777 <johnbarn2409@gmail.com>
2025-07-05 12:39:53 -0700

    Remove binary images

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: { inter: ["Inter", "sans-serif"] },
+      colors: {
+        primary: "#1E40AF",
+        secondary: "#10B981"
+      }
+    },
+  },
+  plugins: [],
+};
